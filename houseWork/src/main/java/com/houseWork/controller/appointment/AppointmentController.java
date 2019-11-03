51269be3a51882911ee3a8264265b693fa7481e8
429565873 <429565873@qq.com>
2019-11-03 21:16:22 +0800

    update

--- houseWork/src/main/java/com/houseWork/controller/appointment/AppointmentController.java
+++ houseWork/src/main/java/com/houseWork/controller/appointment/AppointmentController.java
@@ -35,17 +35,17 @@ public class AppointmentController {
 
     @PostMapping("/add")
     @ApiOperation("添加预约")
-    public ResponseEntity add (
+    public ResponseEntity add(
             @RequestParam(value = "billingType", required = false, defaultValue = "") String billingType,
             @RequestParam(value = "customerId", required = false, defaultValue = "") Integer appointmentId,
             @RequestParam(value = "name", required = false, defaultValue = "") String name,
-            @RequestParam(value = "phone", required = false, defaultValue = "")String phone,
-            @RequestParam(value = "address", required = false, defaultValue = "")String address,
-            @RequestParam(value = "area", required = false, defaultValue = "")String area,
-            @RequestParam(value = "remark", required = false, defaultValue = "")String remark,
-            @RequestParam(value = "clearnerId", required = false, defaultValue = "")Integer cleanerId,
-            @RequestParam(value = "appointmentTime", required = false, defaultValue = "")@ApiParam("预约时间") String appointmentTime
-    ){
+            @RequestParam(value = "phone", required = false, defaultValue = "") String phone,
+            @RequestParam(value = "address", required = false, defaultValue = "") String address,
+            @RequestParam(value = "area", required = false, defaultValue = "") String area,
+            @RequestParam(value = "remark", required = false, defaultValue = "") String remark,
+            @RequestParam(value = "clearnerId", required = false, defaultValue = "") Integer cleanerId,
+            @RequestParam(value = "appointmentTime", required = false, defaultValue = "") @ApiParam("预约时间") String appointmentTime
+    ) {
         Appointment appointment = new Appointment();
         appointment.setBillingType(billingType);
         appointment.setName(name);
@@ -59,7 +59,6 @@ public class AppointmentController {
         CleanerWorkDetail cleanerWorkDetail = new CleanerWorkDetail();
         cleanerWorkDetail.setCid(cleanerId);
         cleanerWorkDetail.setAppointmentId(appointmentId);
-        cleanerWorkDetail.setWorkTime(appointmentTime);
         cleanerService.subscribe(cleanerWorkDetail);
         return new ResponseEntity(ResponseResult.successResponse(appointmentService.getById(appointment.getId())), HttpStatus.OK);
     }
@@ -69,15 +68,15 @@ public class AppointmentController {
     public ResponseEntity all(
             @RequestParam(value = "billingType", required = false, defaultValue = "") String billingType,
             @RequestParam(value = "name", required = false, defaultValue = "") String name,
-            @RequestParam(value = "clearnerId", required = false, defaultValue = "")Integer cleanerId,
+            @RequestParam(value = "clearnerId", required = false, defaultValue = "") Integer cleanerId,
             @RequestParam(value = "page", required = false, defaultValue = "1") int page,
             @RequestParam(value = "size", required = false, defaultValue = "20") int size
-    ){
+    ) {
         Map map = new HashMap();
-        PageHelper.startPage(page,size);
-        map.put("billingType",billingType);
-        map.put("name",name);
-        map.put("cleanerId",cleanerId);
+        PageHelper.startPage(page, size);
+        map.put("billingType", billingType);
+        map.put("name", name);
+        map.put("cleanerId", cleanerId);
         List<Appointment> list = appointmentService.getAll(map);
         PageInfo pageInfo = new PageInfo(list);
         return new ResponseEntity(ResponseResult.successResponse(pageInfo), HttpStatus.OK);
